26e71478df590569779bd338d1925f4bf33c7892
FAY <brunofay1@hotmail.com>
2022-06-20 23:27:48 -0300

    NEW ignitelab day 1

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query{
+    lessons{
+      id
+      title
+    }
+  } 
+`
+type Lesson = {
+  id: string
+  title: string
+}
+
+function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+  return (
+    <ul>
+      {data?.lessons.map(lesson => (<li key={lesson.id}>{lesson.title}</li>))}
+    </ul>
+  )
+}
+
+export default App
